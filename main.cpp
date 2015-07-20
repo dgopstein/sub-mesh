89e79d71bbe1028fddf1b9df96829fcc039666e0
Dan Gopstein <dan@gopstein.com>
2015-07-20 13:32:29 -0400

    Build a TriMesh

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ using namespace glm;
 
 #include "common/shader.hpp"
 #include "common/controls.hpp"
+#include "TriMesh.h"
 
 GLFWwindow* init_glfw_window(int width = 1024, int height = 768, std::string title = "Subdivision Deformation") {
     // Initialise GLFW
@@ -59,7 +60,7 @@ GLFWwindow* init_glfw_window(int width = 1024, int height = 768, std::string tit
 
 // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-static const GLfloat vbd_unit_cube[] = { 
+static const GLfloat unit_cube_vertices[] = { 
         -1.0f,-1.0f,-1.0f,
         -1.0f,-1.0f, 1.0f,
         -1.0f, 1.0f, 1.0f,
@@ -98,104 +99,109 @@ static const GLfloat vbd_unit_cube[] = {
          1.0f,-1.0f, 1.0f
 };
 
+int n_vertices = sizeof(unit_cube_vertices)/sizeof(unit_cube_vertices[0]);
+
 int main( void )
 {
+
+    TriMesh mesh = verts_to_trimesh(unit_cube_vertices, n_vertices);
+
     window = init_glfw_window();
 
-        // Ensure we can capture the escape key being pressed below
-        glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-        glfwSetCursorPos(window, 1024/2, 768/2);
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetCursorPos(window, 1024/2, 768/2);
 
-        // Dark blue background
-        glClearColor(0.5f, 0.5f, 0.7f, 0.0f);
+    // Dark blue background
+    glClearColor(0.5f, 0.5f, 0.7f, 0.0f);
 
-        // Enable depth test
-        glEnable(GL_DEPTH_TEST);
-        // Accept fragment if it closer to the camera than the former one
-        glDepthFunc(GL_LESS); 
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS); 
 
-        // Cull triangles which normal is not towards the camera
-        glEnable(GL_CULL_FACE);
+    // Cull triangles which normal is not towards the camera
+    glEnable(GL_CULL_FACE);
 
-        GLuint VertexArrayID;
-        glGenVertexArrays(1, &VertexArrayID);
-        glBindVertexArray(VertexArrayID);
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
-        // Create and compile our GLSL program from the shaders
-        GLuint programID = LoadShaders( "TransformVertexShader.vert", "TextureFragmentShader.frag" );
-        //GLuint programID = LoadShaders( "phong.vert", "phong.frag" );
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "TransformVertexShader.vert", "TextureFragmentShader.frag" );
+    //GLuint programID = LoadShaders( "phong.vert", "phong.frag" );
 
-        // Get a handle for our "MVP" uniform
-        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-        
-        GLuint vertexbuffer;
-        glGenBuffers(1, &vertexbuffer);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vbd_unit_cube), vbd_unit_cube, GL_STATIC_DRAW);
+    // Get a handle for our "MVP" uniform
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(unit_cube_vertices), unit_cube_vertices, GL_STATIC_DRAW);
 
-        do{
+    do{
 
-                // Clear the screen
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            // Clear the screen
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-                // Use our shader
-                glUseProgram(programID);
+            // Use our shader
+            glUseProgram(programID);
 
-                // Compute the MVP matrix from keyboard and mouse input
-                computeMatricesFromInputs();
-                glm::mat4 ProjectionMatrix = getProjectionMatrix();
-                glm::mat4 ViewMatrix = getViewMatrix();
-                glm::mat4 ModelMatrix = glm::mat4(1.0);
-                glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+            // Compute the MVP matrix from keyboard and mouse input
+            computeMatricesFromInputs();
+            glm::mat4 ProjectionMatrix = getProjectionMatrix();
+            glm::mat4 ViewMatrix = getViewMatrix();
+            glm::mat4 ModelMatrix = glm::mat4(1.0);
+            glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
-                // Send our transformation to the currently bound shader, 
-                // in the "MVP" uniform
-                glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+            // Send our transformation to the currently bound shader, 
+            // in the "MVP" uniform
+            glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-                // 1rst attribute buffer : vertices
-                glEnableVertexAttribArray(0);
-                glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-                glVertexAttribPointer(
-                        0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-                        3,                  // size
-                        GL_FLOAT,           // type
-                        GL_FALSE,           // normalized?
-                        0,                  // stride
-                        (void*)0            // array buffer offset
-                );
+            // 1rst attribute buffer : vertices
+            glEnableVertexAttribArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            glVertexAttribPointer(
+                    0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                    3,                  // size
+                    GL_FLOAT,           // type
+                    GL_FALSE,           // normalized?
+                    0,                  // stride
+                    (void*)0            // array buffer offset
+            );
 
-                // 2nd attribute buffer : UVs
-                glEnableVertexAttribArray(1);
-                glVertexAttribPointer(
-                        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-                        2,                                // size : U+V => 2
-                        GL_FLOAT,                         // type
-                        GL_FALSE,                         // normalized?
-                        0,                                // stride
-                        (void*)0                          // array buffer offset
-                );
+            // 2nd attribute buffer : UVs
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(
+                    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+                    2,                                // size : U+V => 2
+                    GL_FLOAT,                         // type
+                    GL_FALSE,                         // normalized?
+                    0,                                // stride
+                    (void*)0                          // array buffer offset
+            );
 
-                // Draw the triangle !
-                glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+            // Draw the triangle !
+            glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
 
-                glDisableVertexAttribArray(0);
-                glDisableVertexAttribArray(1);
+            glDisableVertexAttribArray(0);
+            glDisableVertexAttribArray(1);
 
-                // Swap buffers
-                glfwSwapBuffers(window);
-                glfwPollEvents();
+            // Swap buffers
+            glfwSwapBuffers(window);
+            glfwPollEvents();
 
-        } // Check if the ESC key was pressed or the window was closed
-        while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-                   glfwWindowShouldClose(window) == 0 );
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+               glfwWindowShouldClose(window) == 0 );
 
-        // Cleanup VBO and shader
-        glDeleteBuffers(1, &vertexbuffer);
-        glDeleteProgram(programID);
-        glDeleteVertexArrays(1, &VertexArrayID);
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteProgram(programID);
+    glDeleteVertexArrays(1, &VertexArrayID);
 
-        // Close OpenGL window and terminate GLFW
-        glfwTerminate();
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
 
-        return 0;
+    return 0;
 }
